7e71c0f8deb41ffd0e8cf4ab429fe1454d4c46c5
Marvin Libam <libam.marvin@gmail.com>
2022-06-25 18:13:49 +0200

    [Tutorial] visuals Done

--- src/components/modal/tutorial/index.tsx
+++ src/components/modal/tutorial/index.tsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import SubPage from "../subpage";
 
 import p1Image from "../../../images/tutorial/pathfinding.png";
-import p2Image1 from "../../../images/tutorial/node_selector.png";
+import p2Image from "../../../images/tutorial/node_selector.png";
+import p2Gif from "../../../images/tutorial/WeightChange.gif";
+import p3Gif from "../../../images/tutorial/WallPlacement.gif";
+import p4Image from "../../../images/tutorial/algorithm_options.png";
+import p5Image from "../../../images/tutorial/additional_commands.png";
+import p6Image from "../../../images/tutorial/tutorial_option.png";
 
 interface TutorialProps {
   closeAction: (state: boolean) => void;
@@ -16,6 +21,9 @@ function Tutorial(props: TutorialProps) {
   };
   const previousAction = () => {
     setPageIdx((prev) => prev - 1);
+  };
+  const doneAction = () => {
+    props.closeAction(false);
   };
 
   return (
@@ -47,7 +55,88 @@ function Tutorial(props: TutorialProps) {
             Use the following buttons to select the type of node to add to the
             grid.
           </div>
-          <img src={p2Image1} alt="node type selection" />
+          <img
+            style={{ marginTop: 10 }}
+            src={p2Image}
+            alt="node type selection"
+          />
+          <div style={{ marginTop: 34 }} id="text">
+            With the weight node selected, clicking on it again will change the
+            node's weight (as displayed on it)
+          </div>
+          <img style={{ marginTop: 13 }} src={p2Gif} alt="weight selection" />
+        </SubPage>
+      )}
+      {pageIdx === 2 && (
+        <SubPage
+          closeButtonAction={props.closeAction}
+          leftFooterButtonText="Previous"
+          leftFooterButtonAction={previousAction}
+          rightFooterButtonText="Next"
+          rightFooterButtonAction={nextAction}
+        >
+          <div id="title">Node Placement</div>
+          <div id="text">
+            Once a node type selected, click on the grid to place it <br />
+            or click and drag to place multiple nodes.
+          </div>
+          <img style={{ marginTop: 10 }} src={p3Gif} alt="wall placement" />
+        </SubPage>
+      )}
+      {pageIdx === 3 && (
+        <SubPage
+          closeButtonAction={props.closeAction}
+          leftFooterButtonText="Previous"
+          leftFooterButtonAction={previousAction}
+          rightFooterButtonText="Next"
+          rightFooterButtonAction={nextAction}
+        >
+          <div id="title">Algorithm visualization</div>
+          <div id="text">
+            To visualize an algorithm, click on the desired option in the
+            header, <br />
+            the animation will then start.
+          </div>
+          <img
+            style={{ marginTop: 70 }}
+            src={p4Image}
+            alt="visualization options"
+          />
+        </SubPage>
+      )}
+      {pageIdx === 4 && (
+        <SubPage
+          closeButtonAction={props.closeAction}
+          leftFooterButtonText="Previous"
+          leftFooterButtonAction={previousAction}
+          rightFooterButtonText="Next"
+          rightFooterButtonAction={nextAction}
+        >
+          <div id="title">Additional commands</div>
+          <div id="text">
+            To use an additionnal command, click on the desired option in the
+            header.
+          </div>
+          <img
+            style={{ marginTop: 77 }}
+            src={p5Image}
+            alt="aditionnal options"
+          />
+        </SubPage>
+      )}
+      {pageIdx === 5 && (
+        <SubPage
+          closeButtonAction={props.closeAction}
+          leftFooterButtonText="Previous"
+          leftFooterButtonAction={previousAction}
+          rightFooterButtonText="Done"
+          rightFooterButtonAction={doneAction}
+        >
+          <div id="title">Tutorial</div>
+          <div id="text">
+            To rewatch this tutorial you can click on the help option.
+          </div>
+          <img style={{ marginTop: 77 }} src={p6Image} alt="Help options" />
         </SubPage>
       )}
     </>
